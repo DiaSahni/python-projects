be65537014d005e342e7f22dad5635846288ed00
DiaSahni <sahni.dia.s@gmail.com>
2025-03-31 18:34:29 -0700

    recipe thing

---
+++ main (3).py
@@ -0,0 +1,17 @@
+def recipe():
+  numberOfBananas = 2
+  NumberOfOil= 1/2
+  NumberOfEggs= 1
+  print("Recipe for banana bread: \n")
+  print("bananas: " + str(numberOfBananas))
+  print("How much flour do you want?")
+  flour = input()
+  print("flour:" + flour)
+  print("Oil: " + str(NumberOfOil))
+  print("How much sugar do you want?")
+  sugar = input()
+  print("sugar:" + sugar)
+  print("Eggs: " + str(NumberOfEggs))
+if __name__ == "__main__":
+  recipe()
+ #do 3 prints, 1 is input#
